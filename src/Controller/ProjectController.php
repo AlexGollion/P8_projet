75a7cf36c2f1d35ce9715bfd152b0a06b178cc97
Alex Gollion <alex.gollion@gmail.com>
2025-03-12 14:14:33 +0100

    Ajout de l'édition des projets

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -5,10 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Project;
 use App\Repository\ProjectRepository;
 use App\Repository\TaskRepository;
 use App\Repository\EmployeeRepository;
+use App\Form\ProjectType;
+use Symfony\Component\HttpFoundation\Request;
+
 
 final class ProjectController extends AbstractController
 {
@@ -24,21 +28,37 @@ final class ProjectController extends AbstractController
         $tasks = $this->taskRepository->findBy(['project' => $project]);
 
         return $this->render('project/project.html.twig', [
-            'controller_name' => 'ProjectController',
             'project' => $project,
             'tasks' => $tasks,
         ]);
     }
     
-    #[Route('/project/edit/{id}', name: 'app_project_update_page', requirements: ['id' => '\d+'], methods: ['GET'])]
-    public function update_page(int $id): Response
+    #[Route('/project/edit/{id}', name: 'app_project_update_page', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    public function update_page(Request $request, int $id, EntityManagerInterface $manager): Response
     {
 
         $project = $this->projectRepository->find($id);
+        $form = $this->createForm(ProjectType::class, $project);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+            foreach($formData->getEmployees() as $employee)
+            {
+                $employeeProjects = $employee->getProject();
+                if(!($employeeProjects->contains($project)))
+                {
+                    $employee->addProject($project);
+                }
+            }
+            $manager->flush();
+            
+            return $this->redirectToRoute('app_project_affichage', ['id' => $project->getId()]);
+        }
 
         return $this->render('project/update.html.twig', [
-            'controller_name' => 'ProjectController',
             'project' => $project,
+            'form' => $form,
         ]);
     }
 }
--- src/Form/ProjectType.php
+++ src/Form/ProjectType.php
@@ -8,18 +8,37 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ProjectType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('titre', TextType::class, [
+            ->add('title', TextType::class, [
                 'label' => 'Titre du projet',
+                'label_attr' => [
+                    'class' => 'required',
+                    'for' => 'projet_name',
+                ],
+                'attr' => [
+                    'required' => true,
+                    'id' => 'id',
+                    'name' => 'projet[name]',
+                ]
             ])
-            ->add('employes', EntityType::class, [
+            ->add('employees', EntityType::class, [
                 'class' => Employee::class,
-                'choice_label' => 'id',
+                'label' => 'Inviter des membres',
+                'label_attr' => [
+                    'for' => 'projet_employes',
+                ],
+                'attr' => [
+                    'id' => 'projet_employes',
+                ],
+                'choice_label' => function(Employee $employee) {
+                    return mb_substr($employee->getFirst_name(), 0, 1) . '' . mb_substr($employee->getName(), 0, 1);
+                },
                 'multiple' => true,
             ])
         ;
@@ -28,7 +47,7 @@ class ProjectType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Projet::class,
+            'data_class' => Project::class,
         ]);
     }
 }
